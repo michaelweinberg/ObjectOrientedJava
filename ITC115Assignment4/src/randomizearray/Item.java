200d7a74e63191d72265fd4c03584bc9e8e01cc7
Michael Weinberg <weinberg4@gmail.com>
2015-03-09 13:38:21 -0700

    Initial Commit
    First Commit for Assignment 4

---
+++ ITC115Assignment4/src/randomizearray/Item.java
@@ -0,0 +1,18 @@
+package randomizearray;
+
+
+public class Item {
+ private String name;
+ 
+ public Item(String name){
+  setName(name);
+ }
+
+ public String getName() {
+  return name;
+ }
+
+ public void setName(String name) {
+  this.name = name;
+ }
+}
