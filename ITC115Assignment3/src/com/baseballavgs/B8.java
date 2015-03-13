7de5a49364796d399d0d4c445d9291be11ed575d
Michael Weinberg <worldweinweb>
2015-03-13 12:01:07 -0700

    Assignment 3 Baseball Averages

---
+++ ITC115Assignment3/src/com/baseballavgs/B8.java
@@ -0,0 +1,21 @@
+package com.baseballavgs;
+import java.util.Scanner;
+public class B8 {
+
+	public static void main(String[] args) {
+		 BaseBall player1 = new BaseBall();
+
+		         new Scanner(System.in);
+		 
+//		         String name = null;
+		 
+		         player1.getStats();
+		 
+		         player1.calcAvg();
+		
+		         player1.printAll();
+
+
+	}
+
+}
---
+++ ITC115Assignment3/src/com/baseballavgs/BaseballAverageProgram.java
@@ -0,0 +1,5 @@
+package com.baseballavgs;
+
+public class BaseballAverageProgram {
+
+}
